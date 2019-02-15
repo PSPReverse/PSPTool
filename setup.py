96cce87c024e7c297c6477213536dff4aa2e30ac
Christian Werling <crwerling@gmail.com>
2019-02-15 15:28:41 +0800

    Extend setup.py to now ship a CLI binary called 'psptool'

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from setuptools import setup
 
 setup(
-   name='psptool',
-   version='2.0',
-   description='psptool is a Swiss Army knife for dealing with AMD binary blobs for the Platform Security Processor '
-               '(PSP) inside BIOS ROM files',
-   author='Christian Werling',
-   author_email='crwerling@gmail.com',
-   packages=['psptool'],
-   install_requires=[],
+    name='psptool',
+    version='2.0',
+    description='psptool is a Swiss Army knife for dealing with AMD binary blobs for the Platform Security Processor '
+                '(PSP) inside BIOS ROM files',
+    author='Christian Werling',
+    author_email='crwerling@gmail.com',
+    packages=['psptool'],
+    scripts=['bin/psptool'],
+    install_requires=['prettytable'],
 )
