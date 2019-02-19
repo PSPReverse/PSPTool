6c32a71b568632a38a9b9303bd07259e91518dcb
Christian Werling <crwerling@gmail.com>
2019-02-19 16:39:49 +0800

    Refine test to use all binaries recursively found in the directory

--- psptool2/test/test_binaries.py
+++ psptool2/test/test_binaries.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 import os
-from psptool2 import PSPTool
+from psptool2 import PSPTool, Blob
 
-path = 'psptool2/test/binaries'
+path = '/Users/cwerling/Git/psptool2/psptool2/test/binaries'
 
-for file in os.listdir(path):
-  filename = os.fsdecode(file)
-  if filename.startswith('.'):
-    continue
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        if file[0] == '.':
+            continue
+        filename = os.path.join(subdir, file)
 
-  print(f'File: {filename}')
-  psp = PSPTool.from_file(f'{path}/{filename}')
-  print(psp.blob.agesa_version)
-  print(f'AMD Public Key: {psp.blob.get_entry_by_type(0).key_id}')
-  print()
-
+        print(f'File: {filename}')
+        try:
+            psp = PSPTool.from_file(f'{filename}')
+            print(psp.blob.agesa_version)
+            print(f'AMD Public Key: {psp.blob.get_entry_by_type(0).key_id}')
+            print()
+        except Blob.NoFirmwareEntryTableError:
+            print('No FET found!')
