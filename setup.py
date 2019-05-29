7c5ecbaec66ac6e9057b15a55abc41a94c070f4b
Christian Werling <crwerling@gmail.com>
2019-05-29 15:38:54 +0200

    Add legacy PSPTrace for CLI usage

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     description='psptool2 is a Swiss Army knife for dealing with AMD binary blobs for the Platform Security Processor '
                 '(PSP) inside BIOS ROM files.',
     author='Christian Werling',
-    author_email='crwerling@gmail.com',
+    author_email='cwerling@posteo.de',
     packages=['psptool2'],
-    scripts=['bin/psptool', 'bin/psptool2'],
+    scripts=['bin/psptool', 'bin/psptool2', 'bin/psptrace'],
     install_requires=['prettytable'],
 )
