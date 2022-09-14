95c0929154585be580beefd93afc6293053627bb
konstantin <konstantin.ausborn@protonmail.com>
2022-09-14 18:06:52 +0200

    Removed unused import.

--- psptool/fet.py
+++ psptool/fet.py
@@ -13,7 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import binascii
 
 from .utils import NestedBuffer
 from .directory import Directory
